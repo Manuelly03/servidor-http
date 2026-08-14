65d61f899cafe9c4f6c28899be8d74a2c4e65e3c
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-08-14 21:15:35 +0000

    edit: exercicio 1 part 2

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -2,26 +2,33 @@ import http from 'node:http'
 import { URL } from 'node:url'
 
 const porta = 3000
-
-const server = http.createServer();
-
+const server = http.createServer()
 const requisicao = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-     const urlObj = new URL(req.url, `http://${req.headers.host}`);
+    res.statusCode = 200
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
 
+    
     if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
         const nome = urlObj.searchParams.get('nome');
-        return res.end(JSON.stringify({"nome": nome}))
+        return res.end(JSON.stringify({ "nome": nome }));
+    } else if (req.method === 'GET' && urlObj.pathname === '/') {
+        return res.end(JSON.stringify({ "data": "Esta é a página inicial." }));
+    } else if (req.method === 'GET' && urlObj.pathname === '/contato') {
+        return res.end(JSON.stringify(
+            { "data": 
+                [{ "telefone": "67 99999-9999", "e-mail": "email@gmail.com" }]
+            }))
     }
-   
-    const nome = urlObj.searchParams.get('nome');
-    console.log(nome);
-    return res.write(JSON.stringify({ "nome": nome }));
+    else if (req.method === 'GET' && urlObj.pathname === '/produtos') {
+        return res.end(JSON.stringify(
+            { "data": 
+                [{ "produto": "mascara capilar", "valor": "R$250,00" }]
+            }))
+    }
+
 
     return res.end(JSON.stringify({ "chave": "valor" }));
-
-
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
     res.end();
 }
@@ -30,4 +37,4 @@ server.on('request', requisicao);
 
 server.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`)
-});			
+});

f762ba4d947c1838f17be8c91f87913269643cdb
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-08-14 18:17:24 +0000

    edit: mudei para es6

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,8 +1,10 @@
-const http = require('node:http')
+import http from 'node:http'
 
 const porta = 3000
 
 const server = http.createServer();
+
+
 
 server.on('request', (req, res) => {
  console.log(`Requisição Recebida!  ${req.method} ${req.url}`);
